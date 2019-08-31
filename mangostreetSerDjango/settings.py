391db014deb522c5e18bb738c5a4ad658f1e35d6
dai3023273 <963826680@qq.com>
2019-08-31 18:13:53 +0800

    asdasdas

--- mangostreetSerDjango/settings.py
+++ mangostreetSerDjango/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT=os.path.join(BASE_DIR,r'static\upfile')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = (
--- notes/models.py
+++ notes/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Notes(models.Model):
-    # id = models.CharField(max_length=50)
+    id = models.CharField(max_length=50,primary_key=True)
     title = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
+    type = models.CharField(max_length=50,default="normal")
     desc = models.CharField(max_length=50)
-    likes = models.IntegerField()
-    cover = models.CharField(max_length=50)
+    likes = models.IntegerField(default=0)
+    cover = models.CharField(max_length=50,)
     user_id = models.CharField(max_length=50)
-    collects = models.IntegerField()
+    collects = models.IntegerField(default=0)
     images = models.CharField(max_length=50)
     class Meta:
         db_table = "notes"
