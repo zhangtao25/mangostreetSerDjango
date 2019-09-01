e11e9d7f1e82db65f099aa26c748db19d4764065
zhangtao25 <zhangtao25@lenovo.com>
2019-09-02 00:51:26 +0800

    upd

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('all/',views.get_all, name = 'get_all'),
     path('<str:id>/',views.get_by_id, name = 'get_by_id')
 ]
