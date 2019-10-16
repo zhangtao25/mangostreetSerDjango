f8b043fb6083ee55a7876a90477cf88a51f4e278
Allen Zhang <tzhangm@ctrip.com>
2019-10-16 17:23:15 +0800

    upd

--- testapi/views.py
+++ testapi/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import json
+import time
 
 # Create your views here.
 
 
 def test(request) :
-    SECRET = '53b9eef72ed85fda0f72954c79f3c9e5'
-    APPID = request.GET.get('APPID')
-    CODE = request.GET.get('CODE')
-    response = requests.get('https://api.weixin.qq.com/sns/oauth2/access_token?appid='+APPID+'&secret='+SECRET+'&code='+CODE+'&grant_type=authorization_code')
-    # 返回响应头
-    # print (response.status_code)
+    time.sleep(5)
+    response = 'sssss'
     return HttpResponse(response)
